950bed7047bfa802b69b20eee4612ee56889b57b
httpscammy <109007099+httpscammy@users.noreply.github.com>
2023-12-01 23:47:39 +0000

    some commit

--- src/components/FavDropdown.jsx
+++ src/components/FavDropdown.jsx
@@ -1,21 +1,33 @@
 import React, { useContext } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { FavoritesContext } from '../Favorites';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 function FavDropdown() {
   const { store } = useContext(FavoritesContext);
 
   return (
-      <Dropdown style={{ position: 'absolute', right: '0' }}>
+      <Dropdown>
         <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Favorites
+          Favorites 
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
           {
               store.favorites.map( ({uid, name}) => {
-                  return <Dropdown.Item key={uid}>{name}</Dropdown.Item>
-              })
+                  return (
+                  <Dropdown.Item key={uid}>
+                    <div style={{padding: '1px'}}>
+                      {name}
+                    </div>
+                    <FontAwesomeIcon
+                      icon={faTrashCan}
+                      className="trash-can"
+                      style={{padding: '1px'}}
+                    />
+                  </Dropdown.Item>
+                  )})
           }
         </Dropdown.Menu>
       </Dropdown>
